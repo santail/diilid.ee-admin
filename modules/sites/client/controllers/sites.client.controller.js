ef3bf4d5cb0a3f2ee6801e0d855bbb8c165c69d9
nikolai muhhin <test@test.ee>
2016-03-06 13:12:33 +0000

    Add reactivate, process actions to site

--- modules/sites/client/controllers/sites.client.controller.js
+++ modules/sites/client/controllers/sites.client.controller.js
@@ -67,7 +67,7 @@ angular.module('sites').controller('SitesController', ['$scope', '$stateParams',
 			$scope.setFormFields(false);
 		};
 
-		$scope.callReprocessing = function (site) {
+		$scope.callCleanup = function (site) {
 			site = Sites.get({
 				siteId: site._id
 			}, function () {
@@ -76,6 +76,61 @@ angular.module('sites').controller('SitesController', ['$scope', '$stateParams',
 					"params": {
 						"site": site.url,
 						"cleanup": true
+					},
+					"queue": "offers_queue",
+					"attempts": null,
+					"timeout": null,
+					"delay": new Date().toISOString(),
+					"priority": 0,
+					"status": "queued",
+					"enqueued": new Date().toISOString()
+				});
+
+				// Redirect after save
+				job.$save(function (response) {
+					$scope.tableParams.reload();
+				}, function (errorResponse) {
+					$scope.error = errorResponse.data.message;
+				});
+			});
+		};
+
+		$scope.callReactivate = function (site) {
+			site = Sites.get({
+				siteId: site._id
+			}, function () {
+				var job = new Jobs({
+					"name": "harvester_run_event",
+					"params": {
+						"site": site.url,
+						"reactivate": true
+					},
+					"queue": "offers_queue",
+					"attempts": null,
+					"timeout": null,
+					"delay": new Date().toISOString(),
+					"priority": 0,
+					"status": "queued",
+					"enqueued": new Date().toISOString()
+				});
+
+				// Redirect after save
+				job.$save(function (response) {
+					$scope.tableParams.reload();
+				}, function (errorResponse) {
+					$scope.error = errorResponse.data.message;
+				});
+			});
+		};
+
+		$scope.callProcess = function (site) {
+			site = Sites.get({
+				siteId: site._id
+			}, function () {
+				var job = new Jobs({
+					"name": "harvester_run_event",
+					"params": {
+						"site": site.url
 					},
 					"queue": "offers_queue",
 					"attempts": null,
